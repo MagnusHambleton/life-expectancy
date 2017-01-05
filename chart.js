03706cc9f07fbb1156ec1216368f9e80de622003
Magnus Hambleton <magnus.hambleton@gmail.com>
2017-01-05 16:29:14 +1100

    added animation and correction

--- chart.js
+++ chart.js
@@ -6,12 +6,12 @@
 
 
 var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    margin = {top: 20, right: 200, bottom: 30, left: 50},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var parseTime = d3.timeParse("%d-%b-%y");
+//var parseTime = d3.timeParse("%d-%b-%y");
 
 var x = d3.scaleTime()
     .rangeRound([0, width]);
@@ -20,44 +20,33 @@ var y = d3.scaleLinear()
     .rangeRound([height, 0]);
 
 var valueline = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+    .x(function(d) { return x(d.age); })
+    .y(function(d) { return y(d.death); });
 
-// var data1 = d3.tsv("data1.tsv", function(d) {
-//     d.date = parseTime(d.date);
-//     d.close = +d.close;
-//     return d;
-// })
+var valueline2 = d3.line()
+    .x(function(d) { return x(d.age); })
+    .y(function(d) { return y(d.deathalt); });
 
 
 
-
-
-
-
-
-
-
-
-d3.tsv("data.tsv", function(d) {
-  d.date = parseTime(d.date);
-  d.close = +d.close;
+d3.csv("Agedata.csv", function(d) {
+  d.age = +d.age;
+  d.death = +d.death;
+  d.deathalt=+d.deathalt
   return d;
 }, function(error,firstdata){
     if (error) throw error;
     
     function move () 
     {
-        //d3.selectAll('.line').transition().duration(500);
-        d3.tsv("data1.tsv", function(d) {
-            d.date = parseTime(d.date);
-            d.close = +d.close;
-            return d;
-        }, function(data1){
-            d3.selectAll('.line')
-                .datum(data1).transition().duration(500)
-                .attr('d',valueline)
-        })
+        // firstdata.forEach(function(d) {
+        //     d.date = parseTime(d.date);
+        //     d.close = +d.close-1;
+        // });
+        d3.selectAll('.line')
+            .datum(firstdata).transition().duration(500)
+            .attr('d',valueline2)
+            // debugger
 
     }
 
@@ -69,13 +58,19 @@ d3.tsv("data.tsv", function(d) {
     }
 
 
-  x.domain(d3.extent(firstdata, function(d) { return d.date; }));
-  y.domain(d3.extent(firstdata, function(d) { return d.close; }));
+  x.domain(d3.extent(firstdata, function(d) { return d.age; }));
+  y.domain(d3.extent(firstdata, function(d) { return d.death; }));
 
   g.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x))
+    // .append("text")
+    //   .attr("fill", "#000")
+    //   .attr("y", height)
+    //   .attr("dx", width)
+    //   .style("text-anchor", "end")
+    //   .text("Age");
 
   g.append("g")
       .attr("class", "axis axis--y")
@@ -86,7 +81,7 @@ d3.tsv("data.tsv", function(d) {
       .attr("y", 6)
       .attr("dy", "0.71em")
       .style("text-anchor", "end")
-      .text("Price ($)");
+      .text("Probability of death (% per year)");
 
   g.append("path")
       .datum(firstdata)
@@ -95,8 +90,13 @@ d3.tsv("data.tsv", function(d) {
     
     d3.selectAll('.line')
         .style('fill','none')
-        .attr('stroke','steelblue')
+        .attr('stroke','#000000')
+        .attr('stroke-width',3)
 
+    g.append("text")
+      .attr("x", width+20)
+      .text("65");
+      .style("font-size",height+"px")
 
     //setup the svg
     var svg = d3.select("svg")
